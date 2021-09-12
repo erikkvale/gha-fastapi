ee80eef642fe9f09f04e7c06141ab4612d83978b
Erik Kvale <eirikval@gmail.com>
2021-09-12 12:53:14 -0500

    Fixed test coverage, I think

--- test_main.py
+++ test_main.py
@@ -1,9 +1,13 @@
 from fastapi.testclient import TestClient
 
-from fapi.main import app
+from fapi.main import app, some_function
 
 client = TestClient(app)
 
+def test_some_function():
+    expected = "What do you want?"
+    actual = some_function()
+    assert expected == actual
 
 def test_read_main():
     response = client.get("/")
